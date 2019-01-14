1502507c40f15dfeca9c3e861445bea3b66b6720
erikvw <ew2789@gmail.com>
2019-01-14 17:03:29 -0600

    add test model and test

--- sample_project/tests/models.py
+++ sample_project/tests/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class MyModel(models.Model):
 
     f1 = models.CharField(max_length=25, null=True)
+
+
+class MyModel2(models.Model):
+
+    f1 = models.CharField(max_length=25, null=True)
--- sample_project/tests/tests.py
+++ sample_project/tests/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 
-from .models import MyModel
+from .models import MyModel, MyModel2
 
 
 class SampleTestCase(TestCase):
     def test_sample(self):
         obj = MyModel.objects.create(f1="erik")
+        self.assertEqual(obj.f1, "erik")
+
+    def test_sample2(self):
+        obj = MyModel2.objects.create(f1="erik")
         self.assertEqual(obj.f1, "erik")
